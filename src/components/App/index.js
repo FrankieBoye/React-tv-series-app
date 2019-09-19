1a881fa212d3731a91b293f580e6a0978e9b198a
FrankieBoye <frankie4@btinternet.com>
2019-09-19 20:14:00 +0100

    refactored code to divide logic between separate components

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,16 +5,6 @@ import 'whatwg-fetch';
 import Series from '../../containers/Series'
 
 class App extends Component {
-  state = {
-    series: []
-  }
-
-  componentDidMount() {
-    fetch('http://api.tvmaze.com/search/shows?q=top-gear')
-    .then((response) => response.json())
-    .then(json => this.setState({ series: json }))
-  }
-
   render() {
     return (
       <div className="App">
@@ -22,7 +12,6 @@ class App extends Component {
           <h1 className="App-title">TV Series List</h1>
         </header>
         <Intro message="Here you can find all of your most loved series" />
-        The length of the series array - {this.state.series.length}
         <Series />
       </div>
     );
--- src/containers/Series/index.js
+++ src/containers/Series/index.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 
 class Series extends Component {
+  state = {
+    series: []
+  }
+
+  componentDidMount() {
+    fetch('http://api.tvmaze.com/search/shows?q=top-gear')
+    .then((response) => response.json())
+    .then(json => this.setState({ series: json }))
+  }
+
   render() {
     return (
-      <div>Series container</div>
+      <div>
+      The length of the series array - {this.state.series.length}
+      </div>
     )
   }
 }
