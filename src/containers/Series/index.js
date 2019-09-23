ce066d3327928db4f33450582bf7b13154880ff7
FrankieBoye <frankie4@btinternet.com>
2019-09-23 12:17:21 +0100

    Added input box and removed hardcoded topgear entry. Can now enter dynamic input using API

--- src/containers/Series/index.js
+++ src/containers/Series/index.js
@@ -6,16 +6,19 @@ class Series extends Component {
     series: []
   }
 
-  componentDidMount() {
-    fetch('http://api.tvmaze.com/search/shows?q=top-gear')
-    .then((response) => response.json())
-    .then(json => this.setState({ series: json }))
-  }
+onSeriesInputChange = e => {
+  fetch(`http://api.tvmaze.com/search/shows?q=${e.target.value}`)
+  .then((response) => response.json())
+  .then(json => this.setState({ series: json }))
+}
 
   render() {
     return (
       <div>
       The length of the series array - {this.state.series.length}
+      <div>
+        <input type="text" onChange={this.onSeriesInputChange} />
+      </div>
       <SeriesList list={this.state.series} />
       </div>
     )
